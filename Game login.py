5b3c8efdff94a82e897136fe94b9e217b199cf68
ILAKIYA KANNAN <ilakiyakannan36@gmail.com>
2025-09-26 15:57:16 +0530

    26/09/2025

---
+++ Game login.py
@@ -0,0 +1,37 @@
+def pin():
+      print("***WELCOME TO PYGAME***")
+      print("***Login Credentials***")
+      User_id=input("Enter your Userid:")
+      correct_pin ="1234"
+      attempts =4 
+      while True:
+        pin= input("Enter the pin:")
+
+        if pin== correct_pin:
+            print("Valid pin")
+            break
+        elif attempts>= 2:
+            print("invalid pin")
+            print("Maximum attempts... Access denied...")
+        else:
+            print("Thank you visit again")
+            break
+def game():
+      pin()
+      import random
+      secret_number = random.randint(1,20)
+      print("***Number Guess***")
+      atp=[]
+      for atp in range(5):  
+            guess = int(input("Take a guess: ",))
+            if guess < secret_number:
+                print("The guess is low... higher number... Try again.")
+            elif guess > secret_number:
+                print("The guess is higher... lower number...Try again.")
+            elif guess == secret_number:
+                print(f"Congratulations! You guessed it right. The number was {secret_number}.")
+            else:
+                print("Too many attempts try again later")
+                break   
+game()
+print("Too many attempts try again later") 
